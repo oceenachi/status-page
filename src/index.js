7d4796607841acbbe3b0f2f146f64c4c3dc67878
oceenachi <phillipfrancisca@gmail.com>
2020-03-14 20:58:27 +0100

    accept modifications

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import StatusPage from "./views/StatusPage";
+import Form from "./views/Form";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <App>
+        <Route exact path="/" component={StatusPage} />
+        <Route exact path="/history" component={History} />
+        <Route exact path="/form" component={Form} />
+      </App>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
