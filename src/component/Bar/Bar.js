f76c12ac0517a1616f206bcb51343a5a883c2c76
oceenachi <phillipfrancisca@gmail.com>
2020-03-14 20:42:31 +0100

    create the bar component

---
+++ src/component/Bar/Bar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Bar() {
+  return (
+    <>
+        <table class="table rounded m-auto">
+          <tbody>
+            <tr style={{background: "#00866e"}}className="d-flex justify-content-between my-5">
+              <td style={{ border: "none", color: "white"}}><h4 className="h4 rounded">All Systems Operational</h4></td>
+              <div>
+                <td style={{ color: "#white", border: "none" }}>                  
+                </td>
+              </div>
+            </tr>
+          </tbody>
+        </table>
+    </>
+  );
+}
+
+export default Bar;
